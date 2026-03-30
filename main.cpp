891c7211307b174a80d30469de1d0f5b63cc4b1a
Numberr1st <88944342+Numberr1st@users.noreply.github.com>
2026-03-30 08:39:25 +0000

    Constructor/Deconstructor

--- main.cpp
+++ main.cpp
@@ -10,6 +10,16 @@ using namespace std;
 #include "monster.h"
 
 int main(int argc, const char * argv[]) {
- 
-    return 0;
+    monster *p;
+
+    monster m1("Tony",18,2);
+    monster m2("Peach"),m4("Kling",30);
+    monster m3("Great");
+    monster x[5]={{"jojo",150},{"JiJi",7}};
+
+    p=new monster("Lulu",170,8); //constructor
+    delete p; // destructor
+
+    return 0; // destructor x,m4,m2,m1
+
 }
--- monster.h
+++ monster.h
@@ -7,15 +7,31 @@
 
 #ifndef monster_h
 #define monster_h
+
 class monster{
 private:
     string name;
     int hp,potion;
 public:
     void Attack(monster &);
-          void heal();
-    monster(string, int,int);
+    void heal();
+    monster(string n="Charles", int=10,int=1); // set standard string(name) int(hp) int (potion)
+    ~monster(); //declare destructor
 };
 
+monster::monster(string n,int h,int p){
+    name = n;
+    h>200 ? hp=200: hp=h;
+    p>10 ? potion=10: potion=p;
+    
+    cout << "Monster " << name <<" เริ่ด"<< endl;
+    cout << "============" << endl;
+}
+
+monster::~monster(){ // destructor and return data rom last to first 
+    cout << "Bye Bye!! " << name << " HP : " <<hp<< " Potion : "<< potion <<endl;
+}
 
 #endif /* monster_h */
+
+
